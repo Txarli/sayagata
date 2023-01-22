ee3e601558687dbea6b08f649f4c9a6ceaabdc20
Aitor Urrutia Aranburu <aitor.urrutia@biko2.com>
2023-01-22 17:43:55 +0100

    feat: mejora SEO y accesibilidad

--- src/pages/genuary19/index.tsx
+++ src/pages/genuary19/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import { Header } from "../../components/Header";
 import loadable from "@loadable/component";
+import { SEO } from "../../components/SEO";
+import { HeadProps, PageProps } from "gatsby";
 
 const Component = loadable(() => import("../../sketches/Genuary19"));
 
-const Page: React.FC = () => {
+const Page: React.FC<PageProps> = ({ path }) => {
   return (
     <>
       <Header />
@@ -14,5 +16,13 @@ const Page: React.FC = () => {
     </>
   );
 };
+
+export const Head: React.FC<HeadProps> = ({ location }) => (
+  <SEO
+    title="Black and white"
+    description="A sketch created for the Genuary 2022 18th day with the prompt: Black and White"
+    pathname={location.pathname}
+  />
+);
 
 export default Page;
