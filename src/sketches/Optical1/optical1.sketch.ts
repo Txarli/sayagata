959b8e89ae62167daa2134298f8c09a9e173658f
Aitor Urrutia Aranburu <aitor.urrutia@biko2.com>
2023-04-16 13:06:56 +0200

    feat: dibuja cuadrados

--- src/sketches/Optical1/optical1.sketch.ts
+++ src/sketches/Optical1/optical1.sketch.ts
@@ -1,11 +1,17 @@
 import P5 from "p5";
 
 export const sketch = (p5: P5) => {
-  const tileSize = 150;
+  const tileSize = 270;
   const ondulationAmplitude = 3;
+
+  let xStartOffset: number;
+  let yStartOffset: number;
 
   p5.setup = () => {
     p5.createCanvas(p5.windowWidth, p5.windowHeight);
+
+    xStartOffset = p5.random(0, 20);
+    yStartOffset = p5.random(0, 20);
 
     p5.noLoop();
   };
@@ -13,37 +19,62 @@ export const sketch = (p5: P5) => {
   p5.draw = () => {
     p5.background(255);
 
-    p5.translate(p5.width / 2, p5.height / 2);
-
     p5.fill(0);
-    p5.beginShape();
+    let isEven = false;
+    for (let y = tileSize / 2 - yStartOffset; y < p5.height; y += tileSize) {
+      for (let x = tileSize / 2 - xStartOffset; x < p5.width; x += tileSize) {
+        if (isEven) {
+          drawOndulatedSquare(x, y);
+        }
 
-    drawVerticalOndulatedLine(-tileSize / 2, tileSize / 2, -tileSize / 2);
-    p5.vertex(tileSize / 2, -tileSize / 2);
-    drawVerticalOndulatedLine(tileSize / 2, -tileSize / 2, tileSize / 2);
-    p5.endShape(p5.CLOSE);
-    p5.point(0, 0);
+        isEven = !isEven;
+      }
+    }
   };
 
   p5.windowResized = function () {
     p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
   };
 
+  function drawOndulatedSquare(x: number, y: number) {
+    p5.push();
+    p5.translate(x, y);
+
+    p5.beginShape();
+    drawVerticalOndulatedLine(-tileSize / 2, tileSize / 2, -tileSize / 2);
+    drawHorizontal(-tileSize / 2, -tileSize / 2, tileSize / 2);
+    drawVerticalOndulatedLine(tileSize / 2, -tileSize / 2, tileSize / 2);
+    drawHorizontal(tileSize / 2, tileSize / 2, -tileSize / 2);
+    p5.endShape(p5.CLOSE);
+    p5.pop();
+  }
+
   function drawVerticalOndulatedLine(x: number, yStart: number, yEnd: number) {
     const direction = yEnd - yStart >= 0 ? 1 : -1;
-    console.log("Direction", direction);
-    const hasToDraw = (y: number) => (direction === 1 ? y <= yEnd : y >= yEnd);
+    const hasToDraw = (y: number) => (direction === 1 ? y < yEnd : y > yEnd);
 
     for (let y = yStart; hasToDraw(y); y += direction * 10) {
-      if (direction === 1) {
-        console.log("Drawing y", y);
-      }
       p5.vertex(
         x -
           direction *
             ondulationAmplitude *
             p5.sin(p5.map(y, yStart, yEnd, 0, p5.TWO_PI)),
         y
+      );
+    }
+  }
+
+  function drawHorizontal(y: number, xStart: number, xEnd: number) {
+    const direction = xEnd - xStart >= 0 ? 1 : -1;
+    const hasToDraw = (x: number) => (direction === 1 ? x < xEnd : x > xEnd);
+
+    for (let x = xStart; hasToDraw(x); x += direction * 10) {
+      p5.vertex(
+        x,
+        y +
+          direction *
+            ondulationAmplitude *
+            p5.sin(p5.map(x, xStart, xEnd, 0, p5.TWO_PI))
       );
     }
   }
