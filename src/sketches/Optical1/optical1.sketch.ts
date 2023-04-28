bb7a8df1df28e5fbc5b81c77f524a1751cbccde7
Aitor Urrutia Aranburu <aitor.urrutia@biko2.com>
2023-04-28 19:24:19 +0200

    feat: dibuja líneas aburridasssss (ya llegará)

--- src/sketches/Optical1/optical1.sketch.ts
+++ src/sketches/Optical1/optical1.sketch.ts
@@ -63,21 +63,10 @@ export const sketch = (p5: P5) => {
     p5.endShape(p5.CLOSE);
 
     p5.fill(255);
-    p5.strokeWeight(0);
+    p5.strokeWeight(4);
+    p5.stroke(255);
 
-    p5.beginShape();
-    p5.vertex(-tileSize / 2, -tileSize / 2);
-
-    for (let x = -tileSize / 2; x <= tileSize / 2; x += 3) {
-      p5.vertex(x, x - 5);
-    }
-
-    p5.vertex(tileSize / 2, tileSize / 2);
-
-    for (let x = tileSize / 2; x >= -tileSize / 2; x -= 3) {
-      p5.vertex(x, x + 5);
-    }
-    p5.endShape(p5.CLOSE);
+    p5.line(-tileSize / 2, -tileSize / 2, tileSize / 2, tileSize / 2);
 
     p5.pop();
   }
